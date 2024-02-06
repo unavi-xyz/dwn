90b9e78e8c57904d3a60583f10e0ed56ce8097df
Kayh <basedlainpilled@gmail.com>
2024-02-06 17:02:36 -0500

    fix db test

--- dwn-server/tests/db.rs
+++ dwn-server/tests/db.rs
@@ -3,7 +3,19 @@ use tracing_test::traced_test;
 
 #[sqlx::test]
 #[traced_test]
-async fn insert_record(pool: Pool<MySql>) {
+async fn insert_record() {
+    let database_url = std::env::var("DATABASE_URL").expect("DATABASE_URL is not set");
+
+    let pool = Pool::<MySql>::connect(database_url.as_str())
+        .await
+        .expect("Failed to connect to database");
+
+    // Clear the table
+    sqlx::query!("DELETE FROM Record")
+        .execute(&pool)
+        .await
+        .expect("Failed to clear table");
+
     sqlx::query!("INSERT INTO Record (id, data) VALUES ('test', 'test')")
         .execute(&pool)
         .await
