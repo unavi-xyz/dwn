a3b16a686af6f91b779860582b570853bc015dd5
Kayh <basedlainpilled@gmail.com>
2024-02-20 18:48:48 -0500

    add delete logic

--- src/handlers/records/delete.rs
+++ src/handlers/records/delete.rs
@@ -1,7 +1,11 @@
 use crate::{
     handlers::{HandlerError, MethodHandler, Reply, Status, StatusReply},
-    message::Message,
+    message::{
+        descriptor::{Descriptor, Filter, FilterDateSort},
+        Message,
+    },
     store::{DataStore, MessageStore},
+    util::encode_cbor,
 };
 
 pub struct RecordsDeleteHandler<'a, D: DataStore, M: MessageStore> {
@@ -12,9 +16,67 @@ pub struct RecordsDeleteHandler<'a, D: DataStore, M: MessageStore> {
 impl<D: DataStore, M: MessageStore> MethodHandler for RecordsDeleteHandler<'_, D, M> {
     async fn handle(
         &self,
-        _tenant: &str,
-        _message: Message,
+        tenant: &str,
+        message: Message,
     ) -> Result<impl Into<Reply>, HandlerError> {
+        message.verify_auth().await?;
+
+        let descriptor = match &message.descriptor {
+            Descriptor::RecordsDelete(desc) => desc,
+            _ => {
+                return Err(HandlerError::InvalidDescriptor(
+                    "Not a RecordsDelete message".to_string(),
+                ));
+            }
+        };
+
+        // TODO: Ensure all immutable values from inital entry are not changed.
+
+        let messages = self
+            .message_store
+            .query(
+                tenant,
+                Filter {
+                    record_id: Some(message.record_id.clone()),
+                    date_sort: Some(FilterDateSort::CreatedDescending),
+                    ..Default::default()
+                },
+            )
+            .await?;
+
+        let active = messages
+            .iter()
+            .find(|m| matches!(m.descriptor, Descriptor::RecordsDelete(_)));
+
+        if let Some(active) = active {
+            let active_desc = match &active.descriptor {
+                Descriptor::RecordsDelete(desc) => desc,
+                _ => {
+                    return Err(HandlerError::InvalidDescriptor(
+                        "Active delete message not a RecordsDelete message?".to_string(),
+                    ));
+                }
+            };
+
+            // If the active delete message is newer, cease processing.
+            if descriptor.message_timestamp < active_desc.message_timestamp {
+                return Ok(StatusReply {
+                    status: Status::ok(),
+                });
+            }
+        }
+
+        // Delete all messages for the record.
+        for m in messages {
+            let block = encode_cbor(&m)?;
+            self.message_store
+                .delete(tenant, block.cid().to_string())
+                .await?;
+        }
+
+        // Store the message.
+        self.message_store.put(tenant, message).await?;
+
         Ok(StatusReply {
             status: Status::ok(),
         })
