5ac124f4120f134e7ad60734ba7848f3469fd0cd
Kayh <basedlainpilled@gmail.com>
2024-03-25 23:41:51 -0400

    clean up

--- flake.nix
+++ flake.nix
@@ -142,6 +142,12 @@
 
               $SHELL
             '';
+
+            S3_ACCESS_KEY_ID = "minioadmin";
+            S3_BUCKET_NAME = "dwn";
+            S3_ENDPOINT = "http://localhost:9000";
+            S3_REGION = "us-east-1";
+            S3_SECRET_ACCESS_KEY = "minioadmin";
           });
         };
       });
