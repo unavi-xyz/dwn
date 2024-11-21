78a3f29f5a4eba26af12768787ae95f12e38510c
Kayh <basedlainpilled@gmail.com>
2024-11-21 08:14:37 -0500

    add data store cleanup test

--- crates/dwn-native-db/src/data_store.rs
+++ crates/dwn-native-db/src/data_store.rs
@@ -118,3 +118,34 @@ impl DataStore for NativeDbStore<'_> {
         Ok(())
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use xdid::core::did::{MethodId, MethodName};
+
+    use super::*;
+
+    #[test]
+    fn test_data_store_cleanup() {
+        let ds = NativeDbStore::new_in_memory().unwrap();
+
+        let target = Did {
+            method_name: MethodName("test".to_string()),
+            method_id: MethodId("test".to_string()),
+        };
+        let cid = &"test cid";
+        let key = (target.to_string(), cid.to_string());
+
+        ds.add_ref(&target, cid, None).unwrap();
+
+        let tx = ds.0.r_transaction().unwrap();
+        assert!(tx.get().primary::<RefCount>(key.clone()).unwrap().is_some());
+        assert!(tx.get().primary::<CidData>(key.clone()).unwrap().is_some());
+
+        ds.remove_ref(&target, cid).unwrap();
+
+        let tx = ds.0.r_transaction().unwrap();
+        assert!(tx.get().primary::<RefCount>(key.clone()).unwrap().is_none());
+        assert!(tx.get().primary::<CidData>(key).unwrap().is_none());
+    }
+}
